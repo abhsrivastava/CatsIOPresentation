62ed24def2b056d03826a6cd8db951717ed8f9bb
Abhishek Srivatsava <abhishes@gmail.com>
2018-09-12 17:01:07 -0500

    added thread shifting

--- src/main/scala/example/CatsIOEx6.scala
+++ src/main/scala/example/CatsIOEx6.scala
@@ -1,29 +1,14 @@
 import cats.effect._
 import cats.syntax.all._
-import java.util.concurrent.ScheduledExecutorService
-import java.util.concurrent.Executors
 import scala.concurrent.duration._
 
-object CatsIOEx6 extends IOApp {
-    def run(args: List[String]) : IO[ExitCode] = {
-        implicit val sc = Executors.newScheduledThreadPool(5)
-        val d = 5 hour
-
-        val ct = IO.cancelable[String] { cb => 
-            val r = new Runnable{
-                def run() = {
-                    Thread.sleep(10000)
-                    cb(Right("foo"))
-                }
-            }
-            val f = sc.schedule(r, d.length, d.unit)
-            IO(f.cancel(true))
-        }
-        val program : IO[Unit] = for {
-            fiber <- ct.start
-            _ <- IO.delay(5 seconds)
-            _ <- fiber.cancel
-        } yield()
-        program.as(ExitCode.Success)
-    }
-}
+// object CatsIOEx6 extends IOApp {
+//     def foo[F[_]: Sync](implicit timer: Timer[F]) = {
+//         F.delay(println("start")) >> timer.sleep(5 seconds) >> F.delay(println("end"))
+//     }
+//     def run(args: List[String]) : IO[ExitCode] = {
+//         val program = foo().start.flatMap{ f => 
+//             f.sleep(2 seconds) >> f.cancel
+//         }
+//     }
+// }
--- src/main/scala/example/CatsIOEx9.scala
+++ src/main/scala/example/CatsIOEx9.scala
@@ -1,8 +1,17 @@
-import cats.effect._
+import cats.effect.{IOApp, IO}
 import cats.syntax.all._
+import scala.concurrent.ExecutionContext
+import java.util.concurrent.Executors
 
-// object CatsIOEx9 extends IOApp {
-//     def run(args: List[String]) : IO[ExitCode] = {
-
-//     }
-// }
+object CatsIOEx9 extends App {
+    val Main = ExecutionContext.global
+    val BlockingIO = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
+    val program = for {
+        _ <- IO { println("what is your name")}
+        _ <- IO.shift(BlockingIO)
+        name <- IO { readLine }
+        _ <- IO.shift(Main)
+    } yield s"Hello $name"
+    val output = program.unsafeRunSync
+    println(output)
+}
