64e5e107acfae4f431f3fe13e3139e50c34dfb0a
Abhishek Srivatsava <abhishes@gmail.com>
2018-09-12 21:22:03 -0500

    added example for IO Cancel

--- src/main/scala/example/CatsIOEx6.scala
+++ src/main/scala/example/CatsIOEx6.scala
@@ -1,14 +1,23 @@
 import cats.effect._
+import scala.concurrent.ExecutionContext
 import cats.syntax.all._
 import scala.concurrent.duration._
 
-// object CatsIOEx6 extends IOApp {
-//     def foo[F[_]: Sync](implicit timer: Timer[F]) = {
-//         F.delay(println("start")) >> timer.sleep(5 seconds) >> F.delay(println("end"))
-//     }
-//     def run(args: List[String]) : IO[ExitCode] = {
-//         val program = foo().start.flatMap{ f => 
-//             f.sleep(2 seconds) >> f.cancel
-//         }
-//     }
-// }
+object CatsIOEx6 extends IOApp {
+    def run(args: List[String]) : IO[ExitCode] = {
+        val launchMissiles = IO(println("Please run to bunker...")) *> IO.sleep(60 seconds) *> IO(println("launched missiles"))
+        val runToBunker = IO.sleep(2 seconds) *> IO(throw new Exception("Can't go back to bunker"))
+        val program = for {
+        fiber <- launchMissiles.start
+        _ <- runToBunker.handleErrorWith { error =>
+            // Retreat failed, cancel launch (maybe we should
+            // have retreated to our bunker before the launch?)
+            fiber.cancel *> IO.raiseError(error)
+        }
+        aftermath <- fiber.join
+        } yield {
+        aftermath
+        }
+        program.as(ExitCode.Success)
+    }
+}
