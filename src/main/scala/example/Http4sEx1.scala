7d4db2a573a2530b1e321f844c6101beba7a1f48
Abhishek Srivatsava <abhishes@gmail.com>
2018-09-12 20:29:56 -0500

    added http4s example

--- src/main/scala/example/Http4sEx1.scala
+++ src/main/scala/example/Http4sEx1.scala
@@ -1,11 +1,10 @@
 import cats.implicits._
 import org.http4s.server.blaze._
 import org.http4s.implicits._
-import cats.effect._
-import org.http4s.dsl.io._
-import org.http4s._
-import scala.concurrent.ExecutionContext.Implicits.global
 import org.http4s.server.Router
+import cats.effect._
+import scala.concurrent.ExecutionContext.Implicits.global
+import org.http4s._, org.http4s.dsl.io._
 
 object Http4sEx1 extends App {
     implicit val cs: ContextShift[IO] = IO.contextShift(global)
@@ -13,7 +12,8 @@ object Http4sEx1 extends App {
     case GET -> Root / "hello" / name =>
         Ok(s"Hello, $name.")
     }
-    val httpApp = Router("/" -> helloWorldService).orNotFound
+    val httpRoute = Router("/" -> helloWorldService)
+    val httpApp : HttpApp[IO] = httpRoute.orNotFound
     val builder = BlazeServerBuilder[IO].bindHttp(8080, "localhost").withHttpApp(httpApp).start
     val server = builder.unsafeRunSync()
 }
